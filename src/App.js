15694cca23027dfa89fa9aa8574d58a27f47a733
Ade-Joshe <femijay9@gmail.com>
2018-11-04 19:50:58 +0100

    started functions, can pass fabrics props now

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+// import Header from './components/header'
+import Sidebar from './components/sidebar';
+import Main from './components/main';
+
 
 class App extends Component {
+
+  state = {
+    fabrics: [
+      {
+        material: {},
+        name: 'Plain'
+      },
+      {
+        material: {},
+        name: 'Black'
+      },
+      {
+        material: {},
+        name: 'Ankara'
+      }
+    ],
+    styleHandler: {},
+    // isFabric: false,
+    setProps: null,
+    Styles: [
+      {
+        name: 'Collar'
+      },
+      {
+        name: 'Sleeves'
+      },
+      {
+        name: 'Cuff'
+      }
+    ]
+  }
+componentWillMount = () => {
+  this.setState({
+    setProps: this.state.fabrics
+  });
+}
+  handleFabric = () => {
+    this.setState({
+
+    })
+  }
+
+  handleStyles = () => {
+    this.setState({
+      setProps: this.state.Styles
+    });
+  }
+
   render() {
+    const { setProps } = this.state;
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+        <div className='firstHeader'>
+          <p><strong> Uwe </strong></p>
+        </div>
+        <div className="header">
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            <span onClick={this.handleFabric}> Fabrics </span>
+            <span onClick={this.handleStyles}> Style </span>
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        </div>
+        <div className='PageWrapper'>
+          <Sidebar fabrics={setProps} />
+          <Main />
+        </div>
       </div>
     );
   }
